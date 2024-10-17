5d0217ffd0038b83a14add857084b6c334a63e67
Gemma Fernandez <gemmazuera@gmail.com>
2024-10-17 18:11:36 +0200

    jueves fin

--- src/Pages/auth/Signup.jsx
+++ src/Pages/auth/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import service from "../../services/config.js"
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"
 
 function Signup() {
   const navigate= useNavigate()
@@ -9,13 +10,36 @@ function Signup() {
   const[email, setEmail]=useState("");
   const [username, setUsername]=useState("");
   const[password, setPassword]=useState("");
-  const[imageProfile, setImageProfile]=useState("")
+ 
   const[errorMessage, setErrorMessage]=useState("")
+  
+  const [imageUrl, setImageUrl] = useState(null);     //cloudinary
+  const [isUploading, setIsUploading] = useState(false);
 
   const handleEmailChange= (e)=>setEmail(e.target.value);
   const handleUsernameChange= (e)=> setUsername(e.target.value);
   const handlePasswordChange=(e)=> setPassword(e.target.value);
-  const handleImageProfileChange=(e)=> setImageProfile(e.target.value);
+  
+
+  const handleFileUpload = async (event) => {
+    if (!event.target.files[0]) {
+      return;
+    }
+    setIsUploading(true);
+  
+    const uploadData = new FormData(); // images and other files need to be sent to the backend in a FormData
+    uploadData.append("image", event.target.files[0]);
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/upload`, uploadData)
+  
+      setImageUrl(response.data.imageUrl);
+      
+      setIsUploading(false);
+    } catch (error) {
+      navigate("/500")
+    }
+  }
+
 
   const handleSignup= async (e)=>{
     e.preventDefault();
@@ -24,7 +48,7 @@ function Signup() {
         email,
         username,
         password,
-        imageProfile
+        profile_image: imageUrl
       }
       await service.post("/auth/signup", newUser)
       navigate("/login")
@@ -39,12 +63,21 @@ function Signup() {
       }
     }
   };
+  
 
   return (
     <div>
       <h2> Form Signup</h2>
       <form onSubmit={handleSignup}>
         <label>Image Profile:</label>
+        <input type="file"
+        name="image"
+        onChange={handleFileUpload}
+        disabled={isUploading}
+        />
+        {imageUrl ? (<div><img src={imageUrl} alt="img" width={200} /></div>) : null}
+        {isUploading ? <h3>... uploading image</h3> : null}
+        
         <br/>
         <label>Username:</label>
         <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
